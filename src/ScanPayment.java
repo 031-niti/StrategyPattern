d7163590cb3ba471a89d9a7960ed54e984ced563
031-niti <earthza6263@gmail.com>
2023-09-25 16:06:39 +0700

    Initial commit

---
+++ src/ScanPayment.java
@@ -0,0 +1,9 @@
+
+public class ScanPayment implements PaymentStrategy {
+
+	@Override
+	public void pay(int amount) {
+		 System.out.println("Pay by scan QR code: " + amount);
+	}
+
+}
---
+++ src/TestDriver.java
@@ -0,0 +1,10 @@
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		ShoppingCart cart1 = new ShoppingCart();
+		cart1.checkout(5500);
+		
+	}
+
+}
